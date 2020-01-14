686ea762ad1b5adc7e87648e2b8519fb7c4ed1de
user1 <huqian.dev@gmail.com>
2020-01-14 22:13:31 +0800

    添加超链接

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,12 @@
     <h1>This is an about page</h1>
     <div></div>
     <span>
-      <a href="#"></a>
+      <a href="#">
+        超链接1
+      </a>
+      <a href="#">
+        超链接2
+      </a>
     </span>
   </div>
 </template>
